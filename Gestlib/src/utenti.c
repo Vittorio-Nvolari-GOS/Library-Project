38e2a8b5627ed15ed5838bd3a5e871d543875e18
Vincenzo-Cutroneo <vincenzo.cutroneo@galileiostiglia.edu.it>
2026-04-20 06:25:20 +0000

    upl

--- Gestlib/src/utenti.c
+++ Gestlib/src/utenti.c
@@ -22,3 +22,37 @@ Lista* crea_lista()
     return l;
 }
 
+#include <stdlib.h>
+#include <string.h>
+#include "../include/utenti.h"
+
+typedef struct Lista 
+{
+    Libro* testa;
+    int lunghezza;
+} Lista;
+
+void carica_lista()
+{
+
+}
+
+Lista* crea_lista() 
+{
+    Lista *l=(Lista*)malloc(sizeof(Lista));
+    l->lunghezza=0;
+    l->testa=NULL;
+    return l;
+}
+
+typedef struct Utente
+{
+    int id;
+    char nome[100];
+    char cognome[100];
+    
+    int anno;
+    float prezzo;
+    struct Libro* next;
+    struct Libro* next_playlist;
+}Utente;
